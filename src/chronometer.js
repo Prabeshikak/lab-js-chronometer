66406146d8ef35bfa552a462ec3d6c49a03e18d9
Prabeshika <prabeshika14@gmail.com>
2023-11-28 17:18:40 +0100

    Mandatory Iterations Completed

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
+    if (this.intervalId === null) {
+      this.intervalId = setInterval(() => {
+        this.currentTime += 1;
+        if (printTimeCallback) {
+          printTimeCallback(this.currentTime);
+        }
+      }, 1000);
+    }
     // ... your code goes here
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
+    return ("0" + value).slice(-2);
     // ... your code goes here
   }
 
   stop() {
-    // ... your code goes here
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
   }
 
   reset() {
+    this.currentTime = 0;
     // ... your code goes here
   }
 
   split() {
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`;
     // ... your code goes here
   }
 }
